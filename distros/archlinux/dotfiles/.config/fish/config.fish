b8d9947f00fef74d88bbc4f246128d5615e944d3
jao <jvlsg@users.noreply.github.com>
2020-04-06 22:11:13 -0300

    Arch w/ wireguard, fish and alacritty updates

---
+++ distros/archlinux/dotfiles/.config/fish/config.fish
@@ -0,0 +1,16 @@
+#FUNCTIONS ======================================================================
+abbr .. "cd .."
+abbr rm "rm -i"
+abbr ll "ls -lah --color=auto"
+abbr l "ls -a --color=auto"
+abbr s "sudo"
+abbr c "clear"
+
+function fonts
+	fc-list | cut -f2 -d ":" | sort -u
+end
+
+# ENV VARIABLES  ============================================================
+#export PATH="/usr/local/sbin:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin:/home/jv/.vimpkg/bin"
+export EDITOR='vim'
+export BROWSER='firefox'
