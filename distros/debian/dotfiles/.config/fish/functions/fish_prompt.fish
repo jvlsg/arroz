0cc54c7205d8daa0d8398802e83b85fd953bc668
jvlsg <jvlsg@noreply.users.github.com>
2020-04-20 15:36:41 -0300

    improved fish prompt in debian

--- distros/debian/dotfiles/.config/fish/functions/fish_prompt.fish
+++ distros/debian/dotfiles/.config/fish/functions/fish_prompt.fish
@@ -1,78 +1,44 @@
-function fish_prompt
-	and set retc green; or set retc red
-    tty|string match -q -r tty; and set tty tty; or set tty pts
+function fish_prompt --description 'Write out the prompt'
+	set laststatus $status
 
-    set_color $retc
-    if [ $tty = tty ]
-        echo -n .-
-    else
-        echo -n '┬─'
-    end
-    set_color -o green
-    echo -n [
-    if test $USER = root -o $USER = toor
-        set_color -o red
-    else
-        set_color -o yellow
-    end
-    echo -n $USER
-    set_color -o white
-    echo -n @
-    if [ -z "$SSH_CLIENT" ]
-        set_color -o blue
-    else
-        set_color -o cyan
-    end
-    echo -n (hostname)
-    set_color -o white
-    #echo -n :(prompt_pwd)
-    echo -n :(pwd|sed "s=$HOME=~=")
-    set_color -o green
-    echo -n ']'
-    set_color normal
-    set_color $retc
-    if [ $tty = tty ]
-        echo -n '-'
-    else
-        echo -n '─'
-    end
-    set_color -o green
-    echo -n '['
-    set_color normal
-    set_color $retc
-    echo -n (date +%X)
-    set_color -o green
-    echo -n ]
-    
-    if type -q acpi
-		if [ (acpi -a 2> /dev/null | string match -r off) ]
-			echo -n '─['
-			set_color -o red
-			echo -n (acpi -b|cut -d' ' -f 4-)
-			set_color -o green
-			echo -n ']'
-		end
-	end
-    echo
-    set_color normal
-    for job in (jobs)
-        set_color $retc
-        if [ $tty = tty ]
-            echo -n '; '
+    if set -l git_branch (command git symbolic-ref HEAD 2>/dev/null | string replace refs/heads/ '')
+        set git_branch (set_color -o red)"$git_branch"
+        if command git diff-index --quiet HEAD --
+            if set -l count (command git rev-list --count --left-right $upstream...HEAD 2>/dev/null)
+                echo $count | read -l ahead behind
+                if test "$ahead" -gt 0
+                    set git_status "$git_status"(set_color red)⬆
+                end
+                if test "$behind" -gt 0
+                    set git_status "$git_status"(set_color red)⬇
+                end
+            end
+            for i in (git status --porcelain | string sub -l 2 | uniq)
+                switch $i
+                    case "."
+                        set git_status "$git_status"(set_color green)✚
+                    case " D"
+                        set git_status "$git_status"(set_color red)✖
+                    case "*M*"
+                        set git_status "$git_status"(set_color green)✱
+                    case "*R*"
+                        set git_status "$git_status"(set_color purple)➜
+                    case "*U*"
+                        set git_status "$git_status"(set_color brown)═
+                    case "??"
+                        set git_status "$git_status"(set_color red)≠
+                end
+            end
         else
-            echo -n '│ '
+            set git_status (set_color green):
         end
-        set_color brown
-        echo $job
+        set git_info "[$git_status$git_branch"(set_color white)"]"
     end
-    set_color normal
-    set_color $retc
-    if [ $tty = tty ]
-        echo -n "'->"
+
+    printf '%s%s%s%s%s%s%s%s%s%s%s%s%s' (set_color -o white) '[' (set_color green) $USER (set_color white)@ (set_color blue)(hostname) (set_color white) ']'  $git_info (set_color white) '[' (set_color yellow) (echo "$PWD" | sed -e "s|^$HOME|~|") (set_color white) ']'
+    if test $laststatus -eq 0
+        printf "%s\n≻%s " (set_color -o green)  (set_color normal)
     else
-        echo -n '╰─>'
+        printf "%s\n≻%s " (set_color -o red) (set_color normal)
     end
-    set_color -o red
-    echo -n '$ '
-    set_color normal
 end
