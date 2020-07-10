014223d065bad460b4352191d6c9fd7536871e6d
jvlsg <jvlsg@users.noreply.github.com>
2020-07-09 22:03:25 -0300

    RSS, Polybar Eye revival

--- distros/archlinux/dotfiles/.config/fish/config.fish
+++ distros/archlinux/dotfiles/.config/fish/config.fish
@@ -16,3 +16,4 @@ set fish_greeting
 #export PATH="/usr/local/sbin:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin:/home/jv/.vimpkg/bin"
 export EDITOR='vim'
 export BROWSER='firefox'
+set PATH $PATH ~/.gem/ruby/2.7.0/bin
