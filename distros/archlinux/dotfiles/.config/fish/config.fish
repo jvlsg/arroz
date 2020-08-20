9c9d0d0694fa3183ad95fb4463695f6dd0214e02
jvlsg <jvlsg@users.noreply.github.com>
2020-08-20 00:40:50 -0300

    Polybar cleanup

--- distros/archlinux/dotfiles/.config/fish/config.fish
+++ distros/archlinux/dotfiles/.config/fish/config.fish
@@ -10,10 +10,12 @@ function fonts
 	fc-list | cut -f2 -d ":" | sort -u
 end
 
+fish_ssh_agent
 set fish_greeting
 
 # ENV VARIABLES  ============================================================
 #export PATH="/usr/local/sbin:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin:/home/jv/.vimpkg/bin"
 export EDITOR='vim'
 export BROWSER='firefox'
+export QT_AUTO_SCREEN_SCALE_FACTOR=0
 set PATH $PATH ~/.gem/ruby/2.7.0/bin
